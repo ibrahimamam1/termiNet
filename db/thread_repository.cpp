42e8fdc7bee7a55adfdec0c66c87471a1d12f73a
ibrahimamam1 <ibrahima59@iut-dhaka.edu>
2024-11-15 22:03:45 +0600

    post Threads Complete

--- db/thread_repository.cpp
+++ db/thread_repository.cpp
@@ -24,12 +24,16 @@ void ThreadRepository::addThreadtoDb(ThreadModel& thread){
         );
 
 
-
+    //get current time and date and convert it to a postgresql compatible type
+    time_t now = time(nullptr);
+    tm *ltm = localtime(&now);
+    char created_at[11]; // YYYY-MM-DD is 10 characters + null terminator
+    strftime(created_at, sizeof(created_at), "%Y-%m-%d", ltm);
 
     q.bindValue(":thread_id", id);
     q.bindValue(":title",  QString::fromStdString(thread.getTitle()));
     q.bindValue(":content", QString::fromStdString(thread.getContent()));
-    q.bindValue(":created_at",   QString::fromStdString(thread.getCreatedAt()));
+    q.bindValue(":created_at",   QString::fromStdString(created_at));
     q.bindValue(":author_id",   QString::number(thread.getAuthorId()));
     q.bindValue(":community_id",   QString::number(thread.getCommunityId()));
     q.bindValue(":parent_thread_id",   QString::number(thread.getParentThreadId()));
